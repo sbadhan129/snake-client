206d922541ca5411d56e8a21e7a70e84694d7d2d
Sourav <souravbadhan909@gmail.com>
2023-05-22 19:03:40 -0400

    Updated play.js.

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const { connect } = require("./client");
 
+const handleUserInput = function (data) {
+  if(data === `\u0003`){
+    console.log("Exit")
+    process.exit();
+  }
+  console.log("Data received:", data)
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
 console.log("Connecting ...");
-connect(); 
+connect();
+setupInput(); 

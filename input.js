14e9426f7da2b0455e668966e50bb40b2b2bb988
Sourav <souravbadhan909@gmail.com>
2023-05-22 19:29:01 -0400

    Updated play.js &  input with movements

--- input.js
+++ input.js
@@ -1,14 +1,31 @@
 const process = require('process');
 
+let connection;
+
+
 const handleUserInput = function (data) {
   if(data === `\u0003`){
     console.log("Exit")
     process.exit();
   }
-  console.log("Data received:", data)
+  switch(data){
+    case 'w':
+      connection.write("Move: up");
+      break;
+      case 'a':
+      connection.write("Move: left");
+      break;
+      case 's':
+      connection.write("Move: down");
+      break;
+      case 'd':
+      connection.write("Move: right");
+      break;
+  }
 };
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");

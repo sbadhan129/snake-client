40725723a50befb7520420329bab087a9a752856
Sourav <souravbadhan909@gmail.com>
2023-05-22 18:38:42 -0400

    My first snake file.

--- play.js
+++ play.js
@@ -1,20 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host:"localhost" ,
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Client and Server is connected")
-  });
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
